778868feba074cd3b9d6909270ffd24ecad5319e
Zain Noman <zain.noman@cowlar.com>
2024-09-05 23:01:03 +0500

    make sure example doesnt need anything from src

--- lib/cutie_mqtt.dart
+++ lib/cutie_mqtt.dart
@@ -1,5 +1,8 @@
 library cutie_mqtt;
 
-export 'src/cutie_mqtt_client.dart' show TcpMqttNetworkConnection,CutieMqttClient;
+export 'src/cutie_mqtt_client.dart' show CutieMqttClient;
 export 'src/connect_packet.dart';
 export 'src/publish_packet.dart' show TxPublishPacket;
+export 'src/byte_utils.dart' show StringOrBytes;
+export 'src/mqtt_events.dart';
+export 'src/mqtt_network_connections.dart';
