2f407fae19aae4b4111e2ef21d0ba25f465b2dd4
Zain Noman <zain.noman@cowlar.com>
2024-09-14 20:38:54 +0500

    pause functionality with dispose

--- lib/src/mqtt_message_queue.dart
+++ lib/src/mqtt_message_queue.dart
@@ -1,22 +1,41 @@
 import 'dart:async';
 
 class MqttMessageQueue {
-  final StreamController<(List<int>, Completer<void>)> _controller =
-      StreamController<(List<int>, Completer<void>)>();
+  final _controller = StreamController<(List<int>, Completer<bool>)>();
+  final _unsentMessages = <(List<int>,Completer<bool>)>[];
+  void _onStreamResume(){
+    for (final msgAndComp in _unsentMessages){
+      _controller.add(msgAndComp);
+    }
+    _unsentMessages.clear();
+  }
 
-  Future<void> addMessage(List<int> message) {
-    final completer = Completer<void>();
-    _controller.add((message,completer));
+  MqttMessageQueue(){
+    _controller.onResume = _onStreamResume;
+  }
+
+  Future<bool> addMessage(List<int> message) {
+    final completer = Completer<bool>();
+    if (_controller.isPaused){
+      _unsentMessages.add((message, completer));
+    }else {
+      _controller.add((message, completer));
+    }
     return completer.future;
   }
 
-  Stream<List<int>> get messageStream => _controller.stream.map((event){
-    final (list,completer) = event;
-    completer.complete();
-    return list;
-  },);
+  Stream<List<int>> get messageStream => _controller.stream.map(
+        (event) {
+          final (list, completer) = event;
+          completer.complete(true);
+          return list;
+        },
+      );
 
-  void dispose(){
+  void dispose() {
     _controller.close();
+    for (final (_,comp) in _unsentMessages){
+      comp.complete(false);
+    }
   }
 }
--- test/mqtt_message_queue_test.dart
+++ test/mqtt_message_queue_test.dart
@@ -39,10 +39,55 @@ void main() {
           expect(fut3Completed, false);
           sub.resume();
           await Future.delayed(const Duration(seconds: 1));
-          expect(messagesReceived, [[0],[1],[2]]);
+          expect(messagesReceived, [
+            [0],
+            [1],
+            [2]
+          ]);
           expect(fut1Completed, true);
           expect(fut2Completed, true);
           expect(fut3Completed, true);
+        },
+      );
+
+      test(
+        "pause functioning with dispose",
+        () async {
+          final MqttMessageQueue q = MqttMessageQueue();
+          final messagesReceived = <List<int>>[];
+          final sub = q.messageStream.listen(messagesReceived.add);
+          sub.pause();
+          bool fut1Completed = false;
+          bool fut2Completed = false;
+          bool fut3Completed = false;
+          bool? fut1Val;
+          bool? fut2Val;
+          bool? fut3Val;
+          q.addMessage([0]).then((value) {
+            fut1Completed = true;
+            fut1Val = value;
+          });
+          q.addMessage([1]).then((value) {
+            fut2Completed = true;
+            fut2Val = value;
+          });
+          q.addMessage([2]).then((value) {
+            fut3Completed = true;
+            fut3Val = value;
+          });
+          await Future.delayed(const Duration(seconds: 1));
+          expect(messagesReceived, isEmpty);
+          expect(fut1Completed, false);
+          expect(fut2Completed, false);
+          expect(fut3Completed, false);
+          q.dispose();
+          await Future.delayed(const Duration(seconds: 1));
+          expect(fut1Completed, true);
+          expect(fut2Completed, true);
+          expect(fut3Completed, true);
+          expect(fut1Val, false);
+          expect(fut2Val, false);
+          expect(fut3Val, false);
         },
       );
     },
